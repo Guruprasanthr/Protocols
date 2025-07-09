b4c887db3a0e9c0969ac7f1df02bbc750d1f3ab5
Guruprasanthr <guruprasanthrsh@gmail.com>
2025-07-09 11:44:34 +0530

    Update spi .v

--- SPI Protocol/spi .v
+++ SPI Protocol/spi .v
@@ -1,126 +1,88 @@
-module spi_master(
-  input clk,
-  input rst,
-  input ss,
-  output reg master_out
-  );  
-  
-  reg [7:0]mem_m;
-  reg [3:0] count;
+module spi_master(input clk,rst,ss,output reg master_out);
+  reg [7:0]master_mem=8'b11010011;
+  reg[3:0]count;
   reg master_in;
   wire master_reg;
-  
-  parameter IDLE     = 1'b0 ,
-  			TRANSMIT = 1'b1 ;
-  
+  parameter idle=1'b0;
+  parameter transmit=1'b1;
   reg state,next_state;
-  
-  //state logic
-  always@(posedge clk or posedge rst)begin
+  always @ (posedge clk or posedge rst)begin
     if(rst)begin
-      state		 <= IDLE;
-      next_state <= IDLE ;
-      count 	 <= 0;
-      mem_m 	 <= 8'b1001_1111;
-      master_out <= 0;
+      state<=idle;
+      next_state<=idle;
+      master_out<=1'b0;
+      count<=1'd7;
     end
-    else
-      state 	 <= next_state;
+      else
+        state<=next_state;
   end
-  
-  //Next_state logic
-  always@(posedge clk)begin
-      case(state)
-        IDLE    :begin
-          		   if(!ss)begin
-                     next_state <= TRANSMIT;
-                     count      <= 4'd8;
-                   end
-          		   else 
-                     next_state <= IDLE;
-        		 end
-       TRANSMIT :begin
-         			master_out <= mem_m[7];
-         			mem_m      <= {mem_m[6:0],master_in};
-                    count      <= count-1;
-         			if(count ==0)begin
-                        count <=  4'd8;
-           				next_state <= IDLE;
-                    end
-                    else begin
-                        count      <= count-1;
-                        next_state <= TRANSMIT;
-                    end
-       			  end
-      endcase
-  end
-  
-  //Instantiation of slave
-  spi_slave ins1(.clk(clk),.rst(rst),.slave_in(master_out),.ss(ss),.slave_out(master_reg ));
-   
-  //Output logic
-  assign master_in = (!ss) ? master_reg : 1'b0;
- 
-
-endmodule
-    
-/////////////////////////////////slave_module////////////////////////////////////////////
-                    
-module spi_slave(
-  input clk,
-  input rst,
-  input slave_in,
-  input ss,
-  output reg slave_out);
-  
-  reg [7:0]mem_s;
-  reg [3:0]count;
-  
-  parameter IDLE 	 = 1'b0,
-  			TRANSMIT = 1'b1;
-  reg state,next_state;
-  
-  //State logic
-  always@(posedge clk)begin
-    if(rst)begin
-      state 	<= IDLE;
-      mem_s 	<= 8'b1111_1110;
-      count 	<= 0;
-      slave_out <= 0;
-    end
-    else
-      state 	<= next_state;
-  end
-  
-  //Next_state_logic
-  always@(posedge clk)begin
+  always @(posedge clk)begin
     case(state)
-      IDLE   :begin
+      idle: begin
+      if(!ss)begin
+        count<=4'd7;
+        next_state<=transmit;
+      end
+        else
+          next_state<=idle;
+      end
+    transmit: begin
+      master_out<=master_mem[7];
+      master_mem<={master_mem[6:0],master_in};
+      if(count==0) begin
+//       	count <= 4'd7; 
+        next_state<=idle;
+      end
+      else begin
+        count<=count-1;
+      next_state<=transmit;
+      end
+    end
+    endcase
+  end
+  spi_slave ins(.clk(clk),.rst(rst),.slave_in(master_out),.ss(ss),.slave_out(master_reg ));
+   
+  
+  assign master_in =master_reg;
+endmodule
+module spi_slave(input clk,rst,ss,slave_in,output reg slave_out);
+    reg [7:0]slave_mem=8'b01011101;
+  reg[3:0]count;
+  wire master_w;
+  parameter idle=1'b0;
+  parameter transmit=1'b1;
+  reg state,next_state;
+  always @ (posedge clk or posedge rst)begin
+    if(rst)begin
+      state<=idle;
+      next_state<=idle;
+      slave_out<=1'b0;
+      count<=1'b0;
+    end 
+      else
+        state<=next_state;
+  end
+  always @(posedge clk)begin
+    case(state)
+      
+  idle   :begin
         		if(!ss)begin
-          			next_state <= TRANSMIT ;
-          			count      <= 4'd9;
+          			count      <= 4'd7;
+          			next_state <= transmit ;
             	end
        			else
-          			next_state <= IDLE;
+          			next_state <= idle;
            	  end
-     TRANSMIT:begin
-          	    slave_out <= mem_s[7];
-       			mem_s     <= {mem_s[6:0],slave_in};
+     transmit:begin
+          	    slave_out <= slave_mem[7];
+       slave_mem    <= {slave_mem[6:0],slave_in};
        			if (count == 0)
-          			next_state <= IDLE;         		   
+          			next_state <= idle;         		   
        		    else begin
-                    next_state <= TRANSMIT;
          			count <= count - 1;
+                    next_state <= transmit;
         		end
               end
     endcase
   end
 endmodule
-                              
-        		
-      
-      
-      
-      
-  
-    
