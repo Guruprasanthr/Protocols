48720cedd903ba8167d3228baf73e91ab9750a83
Guruprasanthr <guruprasanthrsh@gmail.com>
2025-07-09 11:43:03 +0530

    Update spi_tb.v

--- SPI Protocol/spi_tb.v
+++ SPI Protocol/spi_tb.v
@@ -1,34 +1,26 @@
-
-module tb_spi_master_slave;
-
+// Code your testbench here
+// or browse Examples
+module spi;
   reg clk;
+  reg ss;
   reg rst;
-  reg ss; 
-  wire master_out; 
-  
-
-  spi_master uut (
-    .clk(clk),
-    .rst(rst),
-    .ss(ss),
-    .master_out(master_out)
-  );
-
+//   reg master_in;
+  wire master_out;
+  spi_master dut(.clk(clk),.rst(rst),.ss(ss),.master_out(master_out));
   initial begin
-    clk = 1;
-    forever #5 clk=~clk;
+    $dumpfile("waves.vcd");
+    $dumpvars();
+    $monitor("clk=%b,rst=%b,ss=%b,master_in=%b,master_out=%b",clk,ss,master_out);
   end
-  
   initial begin
-    $dumpfile("dump.vcd");
-    $dumpvars;
+    clk=1;
+    forever #5clk=~clk;
   end
-  
-  initial begin
-    rst = 1;ss  = 1;#20;    
-    rst = 0;ss  = 0;#160;   
-    ss = 0;#50;   
-    $finish;
+    initial begin
+    rst=1;ss = 1;
+    #10rst=0;ss = 0;
+    #110 ss=1;
+    #1000$finish;
   end
-
 endmodule
+   
