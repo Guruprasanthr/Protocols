b12622a8efe0fad253d3bebbe255e28ab01aea97
Guruprasanthr <guruprasanthrsh@gmail.com>
2025-07-14 09:45:53 +0530

    Update uart_tb.v

--- UART Protocol/uart_tb.v
+++ UART Protocol/uart_tb.v
@@ -1,11 +1,8 @@
 module tb();
-
-  reg clk, rst;
-  reg [7:0] data;
+reg clk, rst;
+reg [7:0] data;
   
   wire baud_clk, tx, done_t, done_r, error;
-
-  // Instantiate the top-level module
   top_module dut (
     .clk(clk),
     .rst(rst),
@@ -16,29 +13,26 @@ module tb();
     .error(error)
   );
 
-  // Clock generation: 100MHz clock (10ns period)
   initial clk = 0;
   always #5 clk = ~clk;  // 50 MHz toggle rate
 
-  // Stimulus
   initial begin
-    // Dump waveform file
+    
     $dumpfile("dump.vcd");
     $dumpvars(0, tb);
-
-    // Initialize
+ 
     rst = 1;
     data = 8'd0;
     #20;
 
-    // Release reset
+    
     rst = 0;
     #20;
 
-    // Send a data byte
+   
     data = 8'b10010101;
 
-    // Wait long enough for transmission + reception to complete
+   
     #100000;
 
     $finish;
